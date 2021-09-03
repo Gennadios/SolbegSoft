57a1d0f76e0c82aaabe27ee18ac66bd978e2039f
Gennadios <egon.sch@yandex.ru>
2021-09-03 17:21:33 +0300

    added described question and answer classes, added connection string and database context class

--- MillionaireSln/WhoWantsToBeAMillionaire/Models/Answer.cs
+++ MillionaireSln/WhoWantsToBeAMillionaire/Models/Answer.cs
@@ -2,5 +2,8 @@
 {
     public class Answer
     {
+        public int QuestionId { get; set; }
+        public string Content { get; set; }
+        public bool IsCorrect { get; set; }
     }
 }
--- MillionaireSln/WhoWantsToBeAMillionaire/Models/Question.cs
+++ MillionaireSln/WhoWantsToBeAMillionaire/Models/Question.cs
@@ -2,5 +2,7 @@
 {
     public class Question
     {
+        public int Id { get; set; }
+        public string Content { get; set; }
     }
 }
