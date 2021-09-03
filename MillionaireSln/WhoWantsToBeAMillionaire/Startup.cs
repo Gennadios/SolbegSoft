9a60980a55ccfdc4f51e139ea1514ec24b54d329
Gennadios <egon.sch@yandex.ru>
2021-09-03 17:32:36 +0300

    set up connection to database in startup.cs

--- MillionaireSln/WhoWantsToBeAMillionaire/Startup.cs
+++ MillionaireSln/WhoWantsToBeAMillionaire/Startup.cs
@@ -1,20 +1,23 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using WhoWantsToBeAMillionaire.Models;
 
 namespace WhoWantsToBeAMillionaire
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration config) => Configuration = config;
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<MillionaireDbContext>(options =>
+                options.UseSqlServer(Configuration["ConnectionStrings: WhoWantsToBeAMillionaireConnection"]));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
